633e7978c5b0a1d2de8d6c06bbbd67bcfedbf9a0
NameOfFool <ivan.pauk2008@gmail.com>
2025-03-16 21:22:53 +0300

    line chart added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import styles from "./App.module.scss";
 import Status from "./componenst/Status/Status.tsx";
 import {useEffect, useState} from "react";
 import {listen} from "@tauri-apps/api/event";
+import ApexCharts, {ApexOptions} from "apexcharts";
+import ReactApexChart from "react-apexcharts";
 interface SystemStatus {
     cpuUsage: number,
     ramUsage: number,
@@ -9,28 +11,151 @@ interface SystemStatus {
 }
 
 function App() {
+
     async function UpdateStatus(){
         listen("status", (event) =>{
-            setSystem(event.payload as SystemStatus)
+            setSystem((event.payload as SystemStatus))
         })
+    }
+    interface Point{
+        x:Date,
+        y:number
     }
     const [system, setSystem] = useState<SystemStatus>({
         cpuUsage:0,
         ramUsage:0,
         ramTotal:0
     })
+    const [cpuData, setCpuData] = useState<Point[]>([])
+    const [ramData, setRamData] = useState<Point[]>([])
     useEffect(() => {
         UpdateStatus().then( () => "listen begun")
     }, []);
+    useEffect(() => {
+        setCpuData([...cpuData, {x:new Date(Date.now()), y:Math.round(system.cpuUsage)}])
+        setRamData([...ramData, {x:new Date(Date.now()), y:Math.round(system.ramTotal==0?0:100.0 * system.ramUsage/system.ramTotal)}])
+        ApexCharts.exec('cpu', 'updateSeries', [{
+            data: [...cpuData]
+        }])
+        ApexCharts.exec('ram', 'updateSeries', [{
+            data: [...ramData]
+        }])
+    }, [system])
+    const [state, setState] =  useState({
+
+        cpuSeries: [{
+            name:'CPU',
+            data: [...cpuData]
+        }],
+        ramSeries:[{
+            name:'RAM',
+            data:[...ramData]
+        }],
+        cpuOptions: {
+            chart: {
+                id: 'cpu',
+                height: 150,
+                type: 'line',
+                animations: {
+                    enabled: true,
+                    easing: 'linear',
+                    dynamicAnimation: {
+                        speed: 1000
+                    }
+                },
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: false
+                }
+            },
+            colors: ['#546E7A'],
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            title: {
+                text: 'CPU',
+                align: 'left'
+            },
+            markers: {
+                size: 0
+            },
+            xaxis:{
+                type: 'datetime',
+                range: 86400,
+            },
+            yaxis: {
+                max: 100,
+                min:0
+            },
+            legend: {
+                show: false
+            },
+
+        },
+        ramOptions: {
+            chart: {
+                id: 'ram',
+                height: 150,
+                type: 'line',
+                animations: {
+                    enabled: true,
+                    easing: 'linear',
+                    dynamicAnimation: {
+                        speed: 1000
+                    }
+                },
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: false
+                }
+            },
+            colors: ['#00E396'],
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            title: {
+                text: 'RAM',
+                align: 'left'
+            },
+            markers: {
+                size: 0
+            },
+            xaxis: {
+                type: 'datetime',
+                range: 86400,
+            },
+            yaxis: {
+                max: 100,
+                min:0
+            },
+            legend: {
+                show: false
+            },
+        }
+    });
+
+
+
   return (
     <main className={styles.container}>
         <div>
             <h1>Overview</h1>
         </div>
-        <div className={styles.container__stats}>
-        <Status name={"CPU"} value={Math.round(system.cpuUsage)}/>
-        <Status name={"RAM"} value={
-            system.ramTotal==0?0:Math.round(100.0 * system.ramUsage/system.ramTotal)}/>
+        <div className={styles.container__stats} id="cpu">
+            <ReactApexChart options={state.cpuOptions as ApexOptions} series={state.cpuSeries} type="line" height={350} />
+        </div>
+        <div className={styles.container__stats} id="ram">
+            <ReactApexChart options={state.ramOptions as ApexOptions} series={state.ramSeries} type="line" height={350} />
         </div>
     </main>
   );
--- src/componenst/Status/Status.tsx
+++ src/componenst/Status/Status.tsx
@@ -1,15 +1,19 @@
 import styles from "./Status.module.scss"
 import ProgressBar from "../ProgressBar/ProgressBar.tsx";
+import {useEffect, useState} from "react";
+import ApexCharts, {ApexOptions} from 'apexcharts'
+import ReactApexChart from 'react-apexcharts'
 
 interface Props{
-    name:string,
+    options:ApexOptions,
+    series:ApexAxisChartSeries|ApexNonAxisChartSeries,
     value:number
 }
-export default function Status({name, value}:Props){
+export default function Status({options, series, value}:Props){
     return(
         <div className={styles.container}>
-            <span className={styles.container__name}>{name}:</span>
-            <ProgressBar className={styles.container__bar} value={value} max={100} />
+            <div className={styles.container__bar}>
+                <ReactApexChart options={options} series={series} type="line" height={350} /></div>
             <span className={styles.container__percent}>{value}%</span>
         </div>
     )
